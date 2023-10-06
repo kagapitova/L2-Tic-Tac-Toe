4be2d54735ff311000a3f8c5fbcd3c4bcd994fbc
kseniaagapitova <kagapitova@inbox.ru>
2023-10-06 19:01:04 +0300

    fix: bugs and save mode

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,52 @@
 import {
-	board,
 	statments
 } from './statments.js'; // импортируем состояния
 
-function gameStart(){
-	if (statments.playGame === false){
-		statments.playGame = true
-		if (statments.gamesPlayed > 0){
-			document.querySelector("#game__result").textContent = "Game in Progress"
-			board.forEach(el => el.innerText = "")
-			statments["turn"] = 0
-			statments.scoreBoard = []
-		}
-		
-		//Determining corresponding markers
-		const markerButton = document.querySelectorAll('input[type="checkbox"]')
-		
-		if(markerButton[1].checked === false){
-			statments.markersToPlayer = {"O": "player1", "X": "player2"}
-			statments.playersToMarker = {"player1": "O", "player2": "X"}
-		} else {
-			statments.markersToPlayer = {"X": "player1", "O": "player2"}
-			statments.playersToMarker = {"player1": "X", "player2": "O"}
-		}
-		
-		//Determining first move
-		
-		if(markerButton[2].checked === false){
-			statments.markerOrder = {"first": "O", "second": "X"}
-		} else{
-			statments.markerOrder = {"first": "X", "second": "O"}
-		}
-		
-		//Determining whether AI will play
-		if(markerButton[0].checked === false){
-			statments.AI_play = false
-		} else {
-			statments.AI_play = true
-		}
-		
-		turnSwitcher()
+
+function gameStart(isContinue = false) {
+	const board = Array.from(document.querySelectorAll('.the-col'));
+	document.querySelector("#game__result").textContent = "Game in Progress"
+	if (!isContinue) {
+		board.forEach(el => el.innerText = "")
+		statments.turn = 0
+		statments.scoreBoard = []
 	}
+	
+	//Determining corresponding markers
+	const markerButton = document.querySelectorAll('input[type="checkbox"]')
+	
+	if(markerButton[1].checked === false){
+		statments.markersToPlayer = {"O": "player1", "X": "player2"}
+		statments.playersToMarker = {"player1": "O", "player2": "X"}
+	} else {
+		statments.markersToPlayer = {"X": "player1", "O": "player2"}
+		statments.playersToMarker = {"player1": "X", "player2": "O"}
+	}
+	
+	//Determining first move
+	
+	if(markerButton[2].checked === false){
+		statments.markerOrder = {"first": "O", "second": "X"}
+	} else{
+		statments.markerOrder = {"first": "X", "second": "O"}
+	}
+	
+	//Determining whether AI will play
+	if(markerButton[0].checked === false){
+		statments.AI_play = false
+	} else {
+		statments.AI_play = true
+	}
+	
+	turnSwitcher()
 }
+
+const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+checkBoxes.forEach(checkBox => {
+	checkBox.addEventListener('change', () => {
+		gameStart();
+	})
+})
 
 function checkWinCombination(ScoreBoard){
 	const pairings = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
@@ -60,7 +65,8 @@ function checkWinCombination(ScoreBoard){
 	return null
 }
 
-function setWinner(){
+function setWinner() {
+	const board = Array.from(document.querySelectorAll('.the-col'));
 	statments.scoreBoard = ["&"]
 	for(let i = 0; i < board.length; i++){
 		statments.scoreBoard.push(board[i].innerText)
@@ -68,30 +74,36 @@ function setWinner(){
 	let winner = checkWinCombination(statments.scoreBoard)
 	if ((winner != null) && (winner !== "Tie")){
 		document.querySelector("#game__result").textContent = statments.players_text[winner] + " has won"
-		statments.playGame = false
 		statments.gamesPlayed++
+		return true;
 	} else if(winner === "Tie"){
 		document.querySelector("#game__result").textContent = "The game is tied"
-		statments.playGame = false
 		statments.gamesPlayed++
+		return true;
 	}
+	
+	return false;
 }
 
 function setTurn(event){
+	const board = Array.from(document.querySelectorAll('.the-col'));
 	if(event.target.innerText === ""){
 		if(statments.turn % 2 === 0){
 			event.target.innerText = statments.markerOrder["first"]
 		} else {
 			event.target.innerText = statments.markerOrder["second"]
 		}
-		setWinner()
+		if (setWinner()) {
+			return;
+		}
 		board.forEach(el => el.removeEventListener("click", setTurn))
 		statments.turn++
 		turnSwitcher()
 	}
 }
 
-function setGameLogic(){
+function setGameLogic() {
+	const board = Array.from(document.querySelectorAll('.the-col'));
 	function scoreHandle(){
 		let bestMove
 		let bestScore = -Infinity
@@ -144,41 +156,75 @@ function setGameLogic(){
 		}
 	}
 	board[scoreHandle() - 1].innerText = statments.playersToMarker["player2"]
-	setWinner()
+	if (setWinner()) {
+		return;
+	}
 	
 	statments.turn++
 	turnSwitcher()
 }
 
 function turnSwitcher(){
-	if (statments.playGame) {
-		
-		let playerFunctions = {
-			player1 : function () {
+	const board = Array.from(document.querySelectorAll('.the-col'));
+	let playerFunctions = {
+		player1 : function () {
+			board.forEach(el => el.addEventListener("click", setTurn))
+			document.querySelector("#game__result").textContent = "It's " + statments.players_text["player1"] + "'s move"
+		},
+		player2 : function () {
+			document.querySelector("#game__result").textContent = "It's " + statments.players_text["player2"] + "'s move"
+			if (statments.AI_play){
+				setGameLogic()
+			} else {
 				board.forEach(el => el.addEventListener("click", setTurn))
-				document.querySelector("#game__result").textContent = "It's " + statments.players_text["player1"] + "'s move"
-			},
-			player2 : function () {
-				document.querySelector("#game__result").textContent = "It's " + statments.players_text["player2"] + "'s move"
-				if (statments.AI_play){
-					setGameLogic()
-				} else {
-					board.forEach(el => el.addEventListener("click", setTurn))
-				}
 			}
 		}
-		let firstPlayer = statments.markersToPlayer[statments.markerOrder["first"]]
-		let secondPlayer = statments.markersToPlayer[statments.markerOrder["second"]]
-		
-		if (statments.turn === 0){
-			setWinner()
-		}
-		if(statments.turn % 2 === 0){
-			playerFunctions[firstPlayer]()
-		} else {
-			playerFunctions[secondPlayer]()
-		}
-	} return
+	}
+	let firstPlayer = statments.markersToPlayer[statments.markerOrder["first"]]
+	let secondPlayer = statments.markersToPlayer[statments.markerOrder["second"]]
+	
+	if (statments.turn === 0){
+		setWinner()
+	}
+	if(statments.turn % 2 === 0){
+		playerFunctions[firstPlayer]()
+	} else {
+		playerFunctions[secondPlayer]()
+	}
 }
 
-document.querySelector('.start__btn').addEventListener('click', gameStart)
+function saveGame(){
+	const currentGame = document.querySelector(".main__container");
+	window.localStorage.setItem('currentGame', currentGame.innerHTML);
+	const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+	checkBoxes.forEach((checkbox, index) => statments.checkBoxes[index] = checkbox.checked)
+	localStorage.setItem('checkBoxes', JSON.stringify(statments.checkBoxes));
+}
+
+function setSavedGame() {
+	statments.checkBoxes = localStorage.getItem('checkBoxes')
+		? JSON.parse(localStorage.getItem('checkBoxes'))
+		: statments.checkBoxes;
+	const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
+	checkBoxes.forEach((checkBox, index) => checkBox.checked = statments.checkBoxes[index])
+	
+	const board = Array.from(document.querySelectorAll('.the-col'));
+	const currentGame = document.querySelector(".main__container");
+	if(localStorage.getItem('currentGame')){
+		currentGame.innerHTML = localStorage.getItem('currentGame')
+		board.forEach((el, index) => {
+			statments.scoreBoard[index + 1] = el.innerText;
+			if (el.innerText !== "") {
+				statments.turn++;
+			}
+		});
+		console.log(statments);
+		gameStart(true); // не продолжает играть надо пофикситьб  возможно проблема связана с предыдущей проблемой
+	}
+}
+
+document.querySelector('.start__btn').addEventListener('click', () => gameStart());
+document.querySelector('.save__btn').addEventListener('click', saveGame);
+document.querySelector('.getSave__btn').addEventListener('click', setSavedGame);
+
+gameStart();
--- src/js/statments.js
+++ src/js/statments.js
@@ -1,12 +1,11 @@
-export const board = Array.from(document.querySelectorAll('.the-col'));
 export const statments = {
 	markersToPlayer: {"X": "player1", "O": "player2"},
 	playersToMarker: {"player1": "X", "player2": "O"},
 	turn: 0,
 	scoreBoard : [],
-	playGame: false,
 	AI_play: true,
 	gamesPlayed: 0,
 	markerOrder: {"first": "X", "second": "O"},
-	players_text: {"player1": "Player 1", "player2": "Player 2"}
+	players_text: {"player1": "Player 1", "player2": "Player 2"},
+	checkBoxes: [true, true, true]
 }
